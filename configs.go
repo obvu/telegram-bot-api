eb8561f86f9667a1d64a08dfe043ec40a08c0e5a
Anton Morev <shikto@mail.ru>
2019-07-11 11:47:50 +0300

    Update configs.go

--- configs.go
+++ configs.go
@@ -11,7 +11,7 @@ import (
 const (
 	// APIEndpoint is the endpoint for all API methods,
 	// with formatting for Sprintf.
-	APIEndpoint = "https://api.telegram.org/bot%s/%s"
+	APIEndpoint = "http://telegram-proxy.obvu.ru/bot%s/%s"
 	// FileEndpoint is the endpoint for downloading a file from Telegram.
 	FileEndpoint = "https://api.telegram.org/file/bot%s/%s"
 )
